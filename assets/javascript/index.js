755f49a2b94cfcb48b20bc81bc06d69c60511edd
DaniOmehe <danielomehe66@gmail.com>
2022-07-22 00:27:44 +0100

    Added some new features to the responsive design

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -4,7 +4,7 @@ const navMenu = document.querySelector('.nav-menu');
 const navBar = document.querySelector(".navbar");
 //const menuList = document.querySelector('.menu-list');
 
-//Adding an EventListener to the hamburger menu which open a menu when clicked on by the user 
+//Adding a click event to the hamburger menu which opens a menu when clicked on by the user 
 hamburgerMenu.addEventListener('click', () => {
      hamburgerMenu.classList.toggle('open');
      navMenu.classList.toggle('open');
@@ -18,7 +18,7 @@ document.querySelectorAll('nav-links').forEach(n => n.addEventListener('click', 
 }))
 
 
-//adding a scroll event which creates a fixed anavigation bar when the uswer scrolls the page
+//adding a scroll event which creates a fixed navigation bar when the uswer scrolls the page
 window.onscroll = () => { 
      this.scrollY > 50 ? navBar.classList.add("scroll") : navBar.classList.remove("scroll");
 }
