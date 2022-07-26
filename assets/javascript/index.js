56332f5918ec352964b71e7b4bfbf841ee253513
DaniOmehe <danielomehe66@gmail.com>
2022-07-27 00:48:44 +0100

    Added a few new features and maade some changes

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -2,6 +2,7 @@
 const  hamburgerMenu = document.querySelector('.hamburger-menu');
 const navMenu = document.querySelector('.nav-menu');
 const navBar = document.querySelector(".navbar");
+const backToTopButton = document.getElementById('backToTop');
 //const menuList = document.querySelector('.menu-list');
 
 //Adding a click event to the hamburger menu which opens a menu when clicked on by the user 
@@ -20,5 +21,13 @@ document.querySelectorAll('nav-links').forEach(n => n.addEventListener('click', 
 
 //adding a scroll event which creates a fixed navigation bar when the uswer scrolls the page
 window.onscroll = () => { 
+     if(this.scrollY < 500){
+          backToTopButton.classList.add('hide');
+          backToTopButton.classList.remove('show');
+     }else{
+          backToTopButton.classList.add('show');
+          backToTopButton.classList.remove('hide');
+     }
+     
      this.scrollY > 50 ? navBar.classList.add("scroll") : navBar.classList.remove("scroll");
 }
